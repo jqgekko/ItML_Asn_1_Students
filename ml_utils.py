c1b3f3adeb51bbb6adab1474917d535ba77d1911
jqgekko <jqgekko@gmail.com>
2023-01-23 01:45:19 -0500

    Add files via upload

--- ml_utils.py
+++ ml_utils.py
@@ -3,12 +3,10 @@ import numpy as np
 import math
 import sklearn.datasets
 import ipywidgets as widgets
-
-##Seaborn for fancy plots. 
-#%matplotlib inline
 import matplotlib.pyplot as plt
 import seaborn as sns
-plt.rcParams["figure.figsize"] = (8,8)
+plt.rcParams["figure.figsize"] = (8, 8)
+
 
 class edaDF:
     """
@@ -31,7 +29,7 @@ class edaDF:
     -------
     setCat(catList)
         sets the cat variable listing the categorical column names to the list provided in the argument catList
-        
+
         Parameters
         ----------
         catlist : list
@@ -39,7 +37,7 @@ class edaDF:
 
     setNum(numList)
         sets the cat variable listing the categorical column names to the list provided in the argument catList
-        
+
         Parameters
         ----------
         numlist : list
@@ -54,7 +52,7 @@ class edaDF:
             If true, use the hue function in the countplot to split the data by the target value
         show : bool
             If true, display the graphs when the function is called. Otherwise the figure is returned.
-    
+
     histPlots(self, splitTarg=False, show=True)
         generates countplots for the categorical variables in the dataset 
 
@@ -65,24 +63,56 @@ class edaDF:
         show : bool
             If true, display the graphs when the function is called. Otherwise the figure is returned. 
 
+    head(self)
+        Quick overview of the DataFrame  
+
+    nulls(self)
+        Check for missing values
+
+    outliers(self,feature,kde)
+        Detecting Outliers using boxplot and displot.
+
+        Parameters
+        ----------
+        feature : string
+            Feature (column) of the dataset to be plotted.
+        kde : bool
+            If true, display the graph using kernel density estimation .
+
+    descStats(self)
+        Descriptive statistics df.describe()
+
+    dupl(self)
+        Looking for duplicate rows in the dataset.
+
+    explore(self, feature)
+        Explore dataset by feature 
+
+
+
+
     fullEDA()
         Displays the full EDA process. 
     """
+
     def __init__(self, data, target):
         self.data = data
         self.target = target
         self.cat = []
         self.num = []
 
+    def head(self):
+        return self.data.head()
+
     def info(self):
         return self.data.info()
 
     def giveTarget(self):
         return self.target
-        
+
     def setCat(self, catList):
         self.cat = catList
-    
+
     def setNum(self, numList):
         self.num = numList
 
@@ -96,7 +126,8 @@ class edaDF:
             if splitTarg == False:
                 sns.countplot(data=self.data, x=col, ax=ax[r][c])
             if splitTarg == True:
-                sns.countplot(data=self.data, x=col, hue=self.target, ax=ax[r][c])
+                sns.countplot(data=self.data, x=col,
+                              hue=self.target, ax=ax[r][c])
             c += 1
             if c == cols:
                 r += 1
@@ -112,11 +143,12 @@ class edaDF:
         r = 0
         c = 0
         for col in self.num:
-            #print("r:",r,"c:",c)
+            # print("r:",r,"c:",c)
             if splitTarg == False:
                 sns.histplot(data=self.data, x=col, kde=kde, ax=ax[r][c])
             if splitTarg == True:
-                sns.histplot(data=self.data, x=col, hue=self.target, kde=kde, ax=ax[r][c])
+                sns.histplot(data=self.data, x=col,
+                             hue=self.target, kde=kde, ax=ax[r][c])
             c += 1
             if c == cols:
                 r += 1
@@ -125,16 +157,29 @@ class edaDF:
             figure.show()
         return figure
 
+    #
+    def nulls(self):
+        return print(self.data.isnull().sum())
+
+    def outliers(self, feature, kde=False, show=True):
+
+        sns.boxplot(data=self.data, x=feature)
+        return plt.show()
+    #
+
     def fullEDA(self):
         out1 = widgets.Output()
         out2 = widgets.Output()
         out3 = widgets.Output()
         out4 = widgets.Output()
+        out5 = widgets.Output()
 
-        tab = widgets.Tab(children = [out1, out2, out3])
+        tab = widgets.Tab(children=[out1, out2, out3, out4, out5])
         tab.set_title(0, 'Info')
         tab.set_title(1, 'Categorical')
         tab.set_title(2, 'Numerical')
+        tab.set_title(3, 'Nulls')
+        tab.set_title(4, 'Outliers')
         display(tab)
 
         with out1:
@@ -143,7 +188,13 @@ class edaDF:
         with out2:
             fig2 = self.countPlots(splitTarg=True, show=False)
             plt.show(fig2)
-        
+
         with out3:
             fig3 = self.histPlots(kde=True, show=False)
             plt.show(fig3)
+
+        with out4:
+            self.nulls()
+
+        with out5:
+            self.outliers(self.num[0], False, True)
